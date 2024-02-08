2d2e595a428e7bb5b0e5507aca9af7c8c594f718
1234486 <1234486@cegepba.qc.ca>
2024-02-08 11:42:53 -0500

    début code

--- PO1_Encapsulation/Form1.cs
+++ PO1_Encapsulation/Form1.cs
@@ -2,24 +2,54 @@ namespace PO1_Encapsulation
 {
     public partial class Form1 : Form
     {
+
+        Personnage joueur = new();
+        Personnage dragon = new Personnage();  
+
         public Form1()
         {
             InitializeComponent();
+
+            joueur.PointsDeVie = 10;
+            dragon.PointsDeVie = 30;
+
+            joueur.PointsDepuissance = 1;
+            dragon.PointsDepuissance = 3;
+            //Personnage personnage = new Personnage();
+            //personnage.PointDepuissance = 5;
+            //personnage.PoinstDeVie = -10;
+            //personnage.Nom = "Litchy";
+        }
+
+        private void ActualiserLabel() 
+        { 
+            lbl_pointsVieJoueur.Text = "Vie du joueur : " + joueur.PointsDeVie.ToString();
+            lbl_pointsVieDragon.Text = "Vie du dragon : " + dragon.PointsDeVie.ToString();
+
+            lbl_puissanceAttaqueJoueur.Text = "Attaque du joueur : " + joueur.PointsDepuissance.ToString();
+            lbl_puissanceAttaqueDragon.Text = "Attaque du dragon : " + dragon.PointsDepuissance.ToString();
+            
         }
 
         private void btn_attaquer_Click(object sender, EventArgs e)
         {
-
+            dragon.PointsDeVie -= joueur.PointsDepuissance;
+            ActualiserLabel();
         }
 
         private void btn_seSoigner_Click(object sender, EventArgs e)
         {
-
+            ActualiserLabel();
         }
 
         private void btn_puissanceAttaque_Click(object sender, EventArgs e)
         {
+            ActualiserLabel();
+        }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ActualiserLabel();
         }
     }
 }
---
+++ PO1_Encapsulation/Personnage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO1_Encapsulation
+{
+    public class Personnage
+    {
+
+        string nom;
+        int pointsDeVie;
+        int pointsDepuissance;
+
+        //Pour les protéger nos attributs : selectionner, click droit, Action rapide et refactorisation, encapsuler les champs (et utiliser la propriété)
+
+        public string Nom { get => nom; set => nom = value; }
+        public int PointsDeVie
+        {
+            get => pointsDeVie;
+            set
+            {
+                if (value < 0) 
+                { 
+                    pointsDeVie = 0; //Empêche de descendre en bas de 0
+                }
+                else 
+                { 
+                    pointsDeVie = value;
+                }
+            }
+        }
+        public int PointsDepuissance { get => pointsDepuissance; set => pointsDepuissance = value; }
+
+        //public bool EstEnVie()
+        //{
+        //    get
+        //    {
+        //        if(pointsDeVie > 0) 
+        //        { 
+        //            return true;    
+        //        }
+        //        return false;   
+        //    }
+        //}
+    }
+}
